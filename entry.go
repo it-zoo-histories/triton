be0ffddfef015ca29fcd431927469e348e9d3081
kubitre <kubitre@gmail.com>
2019-05-24 04:11:00 +0700

    upd

--- entry.go
+++ entry.go
@@ -1,11 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"strconv"
+	"triton/configuration"
+	"triton/dao"
+	"triton/routes"
 
-const (
-	name = "[HISTORY_CLIEN_SERVICE]:"
+	"github.com/gorilla/mux"
 )
 
+const (
+	nameService            = "[HISTORY_CLIEN_SERVICE]: "
+	configurationSetupPath = "./settings/init.json"
+)
+
+var (
+	configurationService *configuration.InitPackage
+	entryPoint           *routes.EntryRoute
+	serviceDao           *dao.ServiceDAO
+	router               *mux.Router
+)
+
+func configureHost() {
+	configurationService = &configuration.InitPackage{}
+	serviceConfig, err := configurationService.Parse(configurationSetupPath)
+	if err != nil {
+		log.Println(nameService+" error setup init configuration with error code: ", err.Error())
+		return
+	}
+
+	configurationService = serviceConfig
+}
+
+func configureRouter() {
+	router = mux.NewRouter()
+	entryPoint = &routes.EntryRoute{}
+	entryPoint.InitRoute(serviceDao)
+	router = entryPoint.SettingRoute(router)
+}
+
+func configureDao() {
+	serviceDao = &dao.ServiceDAO{}
+}
+
+func init() {
+	configureHost()
+	configureDao()
+	configureRouter()
+}
+
 func main() {
-	fmt.Println(name + " was started")
+	if err := http.ListenAndServe(configurationService.ServerAddress+":"+strconv.Itoa(configurationService.ServerPort), router); err != nil {
+		log.Fatal(nameService+"api was not started, error code: ", err.Error())
+	}
+	fmt.Println(nameService + " was started")
 }
